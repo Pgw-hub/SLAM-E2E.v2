ede56a7d591fb97b6f9d0ef2499cdb229a587661
Pgw-hub <pj012356@gmail.com>
2023-02-09 20:57:26 +0900

    git ignore test

--- build.sh
+++ build.sh
@@ -42,5 +42,5 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j8
 
 # 뉴클레오 보드 연결하지 않은 컴에서 실행할때는 이거 주석처리해줘야함.
-echo "Changing mode /dev/ttyACM0 ..."
-sudo chmod 777 /dev/ttyACM0
+# echo "Changing mode /dev/ttyACM0 ..."
+# sudo chmod 777 /dev/ttyACM0
